ef488eb7ad9fcd27458ca13ad600be41bffe9ea8
Harun42 <43046407+Harun42@users.noreply.github.com>
2025-11-23 00:16:19 +0300

    Create Color.kt

---
+++ web/android/app/src/main/java/com/gogo/mobile/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.gogo.mobile.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GoGoPrimary = Color(0xFFFF5C00)
+val GoGoSecondary = Color(0xFF1E1B4B)
+val GoGoSurface = Color(0xFFFFF6F1)
